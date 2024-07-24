edef6134c2b226f03d7e4e9ff4f186bfa99b58d6
Luis Felix <lfelix3011@gmail.com>
2024-07-24 15:18:12 -0400

    Assigned of constraint names

--- InsertMockData.sql
+++ InsertMockData.sql
@@ -1,5 +1,8 @@
+USE [LectureBSA]
+GO
+
 -- Insert Files
-INSERT INTO [File] (FileName, MIMEType, [Key], URL)
+INSERT INTO [File] ([FileName], [MIMEType], [Key], [URL])
 VALUES 
     ('avatar1.jpg', 'image/jpeg', 'path/to/avatar1.jpg', 'http://example.com/path/to/avatar1.jpg'),
     ('avatar2.jpg', 'image/jpeg', 'path/to/avatar2.jpg', 'http://example.com/path/to/avatar2.jpg'),
@@ -25,11 +28,15 @@ VALUES
 ('UK'),
 ('Canada');
 
+GO
+
 -- Insert Persons
 INSERT INTO [Person] ([FirstName], [LastName], [Biography], [BirthDate], [Gender], [HomeCountryID])
 VALUES
 ('Max', 'Doe', 'John Doe is an actor known for his roles in action films.', '1985-06-15', 'Male', 1),
 ('Maria', 'Smith', 'Jane Smith is a director and actress known for her work in drama.', '1979-11-23', 'Female', 2);
+
+GO
 
 -- Insert Movies
 INSERT INTO [Movie] ([Title], [Description], [Budget], [ReleaseDate], [Duration], [DirectorID], [CountryProducedID], [PosterID])
@@ -38,6 +45,7 @@ VALUES
 	('Drama Movie', 'A heartfelt drama about family and relationships.', 80000000.00, '2023-03-10', 120, 2, 2, 4),
 	('Action Movie', 'An epic action movie with thrilling sequences.', 150000000.00, '2023-05-15', 135, 1, 1, 3);
 
+GO
 
 -- Insert Genre
 INSERT INTO [Genre] ([Name])
@@ -45,6 +53,7 @@ VALUES
 ('Action'),
 ('Drama');
 
+GO
 
 -- Insert Movies Genres 
 INSERT INTO [MovieGenre] ([MovieID], [GenreID])
@@ -52,6 +61,7 @@ VALUES
 (1, 1),
 (2, 2);
 
+GO
 
 -- Insert Persons Photos 
 INSERT INTO [PersonPhoto] ([PersonID], [PhotoID], [IsProfile])
@@ -60,6 +70,7 @@ VALUES
 (1, 5, 0),
 (2, 2, 0);
 
+GO
 
 -- Insert Users Favorite Movies
 INSERT INTO [FavoriteMovies] ([MovieID], [UserID])
@@ -67,6 +78,7 @@ VALUES
 (1, 1),
 (2, 1)
 
+GO
 
 -- Insert Characters
 INSERT INTO [Character] ([Name], [Description], [Role], [MovieID])
@@ -74,9 +86,10 @@ VALUES
 ('Hero', 'The main protagonist of the action movie.', 'leading', 1),
 ('Villain', 'The antagonist in the action movie.', 'supporting', 1);
 
+GO
 
 -- Insert Movie Actors
-INSERT INTO [MovieActor] ([MovieID], [ActorID], [CharacterID], [CreatedAt], [UpdatedAt])
+INSERT INTO [MovieActor] ([MovieID], [ActorID], [CharacterID])
 VALUES
 (1, 1, 1),
 (2, 2, NULL);
