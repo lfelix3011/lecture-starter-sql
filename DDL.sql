15975882cd919482794e90646fa940f3e75ed085
Luis Felix <lfelix3011@gmail.com>
2024-07-24 09:37:59 -0400

    Adding property to store time of creation and update
    Adjust in names and order of table

--- DDL.sql
+++ DDL.sql
@@ -10,7 +10,9 @@ CREATE TABLE [File] (
 	[MimeType] VARCHAR(255) NOT NULL,
 	[Key] VARCHAR(50) NOT NULL,
 	[URL] VARCHAR(300) NOT NULL,
-)
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
+);
 
 CREATE TABLE [User] (
 	[ID] INT IDENTITY(1,1) CONSTRAINT pk_User PRIMARY KEY,
@@ -19,12 +21,16 @@ CREATE TABLE [User] (
 	[LastName] VARCHAR(30) NOT NULL,
 	[Email] VARCHAR(60) NOT NULL,
 	[Password] VARCHAR(200) NOT NULL,
-	[AvatarID] INT CONSTRAINT fk_File_User FOREIGN KEY REFERENCES [File](ID),--Avatar Image
+	[AvatarID] INT CONSTRAINT fk_File_User FOREIGN KEY REFERENCES [File](ID),--Avatar Image,
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
 CREATE TABLE [Country] (
 	[ID] INT IDENTITY(1,1) CONSTRAINT pk_Country PRIMARY KEY,
-	[Name] VARCHAR(80) NOT NULL,
+	[Name] VARCHAR(128) NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
 CREATE TABLE [Person] (
@@ -37,16 +43,16 @@ CREATE TABLE [Person] (
 	[HomeCountryID] INT NOT NULL CONSTRAINT fk_Country_Person FOREIGN KEY REFERENCES [Country](ID),
 	[ProfileID] INT CONSTRAINT fk_File_Person FOREIGN KEY REFERENCES [File](ID),--Profile Image
 	[PhotoID] INT CONSTRAINT fk_File_Person FOREIGN KEY REFERENCES [File](ID),--Photo Image
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
 CREATE TABLE [Person_Photos] (
 	[ID] INT IDENTITY(1,1) CONSTRAINT pk_Person_Photos PRIMARY KEY,
 	[PersonID] INT NOT NULL CONSTRAINT fk_Person_Person_Photos FOREIGN KEY REFERENCES [Person](ID),
 	[PhotoID] INT CONSTRAINT fk_File_Person FOREIGN KEY REFERENCES [File](ID),--Photo Image
-)
-CREATE TABLE [MovieGenre] (
-	[ID] INT IDENTITY(1,1) CONSTRAINT pk_MovieGenre PRIMARY KEY,
-	[Name] VARCHAR(20) NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
 CREATE TABLE [Movie] (
@@ -59,23 +65,38 @@ CREATE TABLE [Movie] (
 	[DirectorID] INT NOT NULL CONSTRAINT fk_Person_Movie FOREIGN KEY REFERENCES [Person](ID),--Director Table  (Not Sure if a Movie can have multiple directors)
 	[CountryProducedID] INT NOT NULL CONSTRAINT fk_Country_Movie FOREIGN KEY REFERENCES [Country](ID),
 	[PosterID] INT CONSTRAINT fk_File_Movie FOREIGN KEY REFERENCES [File](ID),--Poster Image
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
-CREATE TABLE [Movies_Genres] ( --Evaluate if need to be a CHECK
-	CONSTRAINT pk_Movies_Genres PRIMARY KEY ([GenreID], [MovieID]),
-	[GenreID] INT NOT NULL CONSTRAINT fk_MovieGenre_Movies_Genres FOREIGN KEY REFERENCES [MovieGenre](ID),--Genre
-	[MovieID] INT NOT NULL CONSTRAINT fk_Movie_Movies_Genres FOREIGN KEY REFERENCES [Movie](ID),--Genre
+CREATE TABLE [Genre] (
+	[ID] INT IDENTITY(1,1) CONSTRAINT pk_Genre PRIMARY KEY,
+	[Name] VARCHAR(20) NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
+)
+
+CREATE TABLE [Movie_Genre] (
+	CONSTRAINT pk_Movie_Genre PRIMARY KEY ([MovieID], [GenreID]),
+	[MovieID] INT NOT NULL CONSTRAINT fk_Movie_Movie_Genre FOREIGN KEY REFERENCES [Movie](ID),
+	[GenreID] INT NOT NULL CONSTRAINT fk_Genre_Movie_Genre FOREIGN KEY REFERENCES [Genre](ID),
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 
 CREATE TABLE [FavoriteMovies] (
 	[ID] INT IDENTITY(1,1) CONSTRAINT pk_Person_Photos PRIMARY KEY,
 	[MovieID] INT NOT NULL CONSTRAINT fk_Movie_FavoriteMovies FOREIGN KEY REFERENCES [Movie](ID),
 	[LikeCounter] INT NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 CREATE TABLE [FavoriteMovies_User] (
 	[ID] INT IDENTITY(1,1) CONSTRAINT pk_FavoriteMovies_User PRIMARY KEY,
 	[UserID] INT NOT NULL CONSTRAINT fk_User_FavoriteMovies FOREIGN KEY REFERENCES [User](ID),
 	[FavoriteMovieID] INT NOT NULL CONSTRAINT fk_FavoriteMovies_FavoriteMovies_User FOREIGN KEY REFERENCES [FavoriteMovies](ID),
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
 --CREATE TABLE [CharacterRole] ( --Evaluate if need to be a CHECK
 --	[ID] INT IDENTITY(1,1) PRIMARY KEY,
@@ -91,4 +112,6 @@ CREATE TABLE [Character] (
 	[Role] VARCHAR(60) NOT NULL CHECK ([Role] IN ('leading', 'supporting', 'background')),
 	[MovieID] INT NOT NULL CONSTRAINT fk_Movie_Character FOREIGN KEY REFERENCES [Movie](ID),--Movie
 	[ActorID] INT CONSTRAINT fk_Person_Character FOREIGN KEY REFERENCES [Person](ID),--Actor (May need another table)
+	[CreatedAt] DATETIME NOT NULL,
+	[UpdatedAt] DATETIME NOT NULL,
 )
